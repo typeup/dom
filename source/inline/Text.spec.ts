239e5e2a70d1eedbf218f18d4c7f4bb524389bad
Simon Mika <simon@mika.se>
2019-01-30 22:24:11 +0100

    Added more tests.

--- source/inline/Text.spec.ts
+++ source/inline/Text.spec.ts
@@ -1,9 +1,19 @@
 import * as inline from "."
 
+const node = new inline.Text("value")
+
 describe("Text", () => {
-	it("create", () => {
-		const node = new inline.Text("value")
+	it("properties", () => {
 		expect(node.class).toBe("Text")
 		expect(node.value).toBe("value")
+	})
+	it("toObject", () => {
+		expect(node.toObject()).toEqual({
+			class: "Text",
+			value: "value",
+		})
+	})
+	it("toString", () => {
+		expect(node.toString()).toEqual("value")
 	})
 })
