de87a460dc9623fc87e144d434ca0857b0329375
Simon Mika <simon@mika.se>
2019-02-24 15:30:33 +0100

    Added Node to export.

--- source/index.ts
+++ source/index.ts
@@ -2,10 +2,12 @@ import * as inline from "./inline"
 import * as block from "./block"
 import { Document } from "./Document"
 import { File } from "./File"
+import { Node } from "./Node"
 
 export {
 	inline,
 	block,
 	Document,
 	File,
+	Node,
 }
