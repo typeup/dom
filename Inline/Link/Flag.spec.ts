e7b173fb79b68497df1e5fa2e253fb2baba9a1d3
Simon Mika <simon@mika.se>
2026-05-08 08:42:22 +0200

    Added flag is function.

---
+++ Inline/Link/Flag.spec.ts
@@ -0,0 +1,15 @@
+import { Flag } from "./Flag"
+
+describe("Flag", () => {
+	it.each([
+		{ input: "blank", expected: true },
+		{ input: "download", expected: true },
+		{ input: "invalid", expected: false },
+		{ input: "", expected: false },
+		{ input: 123, expected: false },
+		{ input: null, expected: false },
+		{ input: undefined, expected: false },
+		{ input: {}, expected: false },
+		{ input: [], expected: false }
+	])("is $input", ({ input, expected }) => expect(Flag.is(input)).toBe(expected))
+})
--- Inline/Link/Flag.ts
+++ Inline/Link/Flag.ts
@@ -2,4 +2,7 @@ export type Flag = (typeof Flag.values)[number]
 
 export namespace Flag {
 	export const values = ["blank", "download"] as const
+	export function is(value: unknown | Flag): value is Flag {
+		return typeof value === "string" && values.includes(value as Flag)
+	}
 }
